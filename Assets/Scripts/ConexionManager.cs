f6d8df1d7a8b4aefd293a7bfba8c849e7c82ce82
EFDesarollo <efermal235@g.educaand.es>
2022-11-08 12:46:53 +0100

    Photon_Lesson_3

--- Assets/Scripts/ConexionManager.cs
+++ Assets/Scripts/ConexionManager.cs
@@ -8,6 +8,7 @@ using WebSocketSharp;
 using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 using Photon.Pun.Demo.Cockpit.Forms;
+using System.ComponentModel;
 
 public class ConexionManager : MonoBehaviourPunCallbacks
 {
@@ -17,15 +18,16 @@ public class ConexionManager : MonoBehaviourPunCallbacks
     #region Public Vars
     [Header("Paneles UI")] // paneles del menú
     public GameObject initialPanel;
-    public GameObject welcomePanel, createRoomPanel;
+    public GameObject welcomePanel, createRoomPanel, roomPanel, joinRoomPanel;
     [Header("Botones UI")] // Boton de conexion
     public Button connectButton;
     [Header("Inputs UI")] // Input del usuario
     public TMP_InputField nicknameInput;
-    public TMP_InputField roomNameInput, minPlayersInput, maxPlayersInput;
+    public TMP_InputField roomNameInput, minPlayersInput, maxPlayersInput, joinRoomNameInput;
     [Header("Info UI")] // Objetos que contendrán información del y para el jugador
     public TextMeshProUGUI stateText;
     public TextMeshProUGUI userText;
+    public TextMeshProUGUI roomUserNikcname, roomName, roomPlayersCount, roomType;
     private void Start()
     {
         SetActivePanel(initialPanel);
@@ -52,6 +54,27 @@ public class ConexionManager : MonoBehaviourPunCallbacks
         roomOptions.IsOpen = isOpen;
         PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
     }
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+    public void ModifyRoomPanel()
+    {
+        // activate panel
+        SetActivePanel(roomPanel);
+        //actualizar informacion del panel
+        //comenzamos leyendo la información.
+        Player[] jugadores = PhotonNetwork.PlayerList;
+        string playersText = "";
+        for (int i = 0; i < jugadores.Length; i++)
+        {
+            playersText += jugadores[i].ActorNumber + " - " + jugadores[i].NickName + "\n";
+        }
+        roomUserNikcname.text = playersText;
+        roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+        roomPlayersCount.text = "Players: " + PhotonNetwork.CurrentRoom.PlayerCount + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
+        roomType.text = "Room Type: " + (PhotonNetwork.CurrentRoom.IsOpen ? "Open" : "Closed");
+    }
     #region Override functions
 
     public override void OnConnected()
@@ -66,11 +89,39 @@ public class ConexionManager : MonoBehaviourPunCallbacks
     {
         //base.OnCreatedRoom();
         ChangeState("Room Created Susefully");
+        ModifyRoomPanel();
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         //base.OnCreateRoomFailed(returnCode, message);
         ChangeState("Create room failed: " + message);
+    }
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        // Informamos al usuario
+        ChangeState("Joined!");
+        ModifyRoomPanel();
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        // Informamos al usuario
+        ChangeState("Join Failed");
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        // Informamos al usuario
+        ChangeState("a player joined!");
+        ModifyRoomPanel();
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        // Informamos al usuario
+        ChangeState("a player Left!");
+        ModifyRoomPanel();
     }
     #endregion
     #region On Click Events
@@ -89,6 +140,7 @@ public class ConexionManager : MonoBehaviourPunCallbacks
         else
             ChangeState("There isn't a nickname");
     }
+   
     public void OnClickCreateRoom()
     {
         if (!(string.IsNullOrEmpty(roomNameInput.text) || string.IsNullOrWhiteSpace(roomNameInput.text)))
@@ -102,6 +154,14 @@ public class ConexionManager : MonoBehaviourPunCallbacks
                 ChangeState("Players Input Error");
         }
     }
+    public void OnClickJoinRoom()
+    {
+        if (!(string.IsNullOrEmpty(joinRoomNameInput.text) || string.IsNullOrWhiteSpace(joinRoomNameInput.text)))
+            JoinRoom(joinRoomNameInput.text);
+        else
+            // Informamos al usuario
+            ChangeState("name error");
+    }
     #endregion
     #region Active Panels
     private void SetActivePanel(GameObject panel)
@@ -109,6 +169,8 @@ public class ConexionManager : MonoBehaviourPunCallbacks
         initialPanel.SetActive(false);
         welcomePanel.SetActive(false);
         createRoomPanel.SetActive(false);
+        roomPanel.SetActive(false);
+        joinRoomPanel.SetActive(false);
 
         panel.SetActive(true);
     }
@@ -123,6 +185,10 @@ public class ConexionManager : MonoBehaviourPunCallbacks
     public void SetActivePanelCreateRoom()
     {
         SetActivePanel(createRoomPanel);
+    }
+    public void SetActivePanelJoinRoom()
+    {
+        SetActivePanel(joinRoomPanel);
     }
     #endregion
 }
